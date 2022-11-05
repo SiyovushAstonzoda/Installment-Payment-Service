10e4f15a27a90f1d30a7065c613f915f54d89bd3
Siyovush Astonzoda <astonov77@gmail.com>
2022-11-05 19:16:09 +0500

    first commit

---
+++ Infrastructure/Context/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+namespace Infrastructure.Context;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //CourseAssignment Many to Many Course/Instructor
+        modelBuilder.Entity<Order>()
+            .HasOne(c => c.Customer)
+            .WithMany(o => o.Orders)
+            .HasForeignKey(ci => ci.CustomerId);
+
+        modelBuilder.Entity<Order>()
+            .HasOne(i => i.Installment)
+            .WithMany(o => o.Orders)
+            .HasForeignKey(ii => ii.InstallmentId);
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; } //Many to Many
+    public DbSet<Installment> Installments { get; set; }
+    public DbSet<Product> Products { get; set; }
+}
